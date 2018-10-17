585baff0da8962e83378a210227a698c79e8421d
Dexter Fichuk <dexterfichuk@gmail.com>
2018-10-17 07:29:50 -0400

    First commit, working on mock numpy

---
+++ capsule_model.py
@@ -0,0 +1,60 @@
+
+from keras import layers, models
+from keras import backend as K
+from capsule_layers import ConvCapsuleLayer, Length, Mask
+K.set_image_data_format('channels_last')
+
+def CapsNetBasic(input_shape, n_class=2):
+    x = layers.Input(shape=input_shape)
+
+    # Layer 1: Just a conventional Conv2D layer
+    conv1 = layers.Conv3D(filters=1, kernel_size=5, strides=1, padding='same', activation='relu', name='conv1')(x)
+
+    # Reshape layer to be 1 capsule x [filters] atoms
+    print('Conv 1 Shape:', conv1.get_shape())
+    _, H, W, D, C = conv1.get_shape()
+    conv1_reshaped = layers.Reshape((H.value, W.value, D.value, 1, C.value))(conv1)
+    print('Conv Reshaped Shape:', conv1_reshaped.get_shape())
+
+    # Layer 1: Primary Capsule: Conv cap with routing 1
+    primary_caps = ConvCapsuleLayer(kernel_size=5, num_capsule=8, num_atoms=32, strides=1, padding='same',
+                                    routings=1, name='primarycaps')(conv1_reshaped)
+    print("Primary Caps", primary_caps.get_shape())
+    # Layer 4: Convolutional Capsule: 1x1
+    seg_caps = ConvCapsuleLayer(kernel_size=1, num_capsule=1, num_atoms=16, strides=1, padding='same',
+                                routings=3, name='seg_caps')(primary_caps)
+
+    # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
+    out_seg = Length(num_classes=n_class, seg=True, name='out_seg')(seg_caps)
+    print('Out Seg', out_seg.get_shape())
+    # Decoder network.
+    _, H, W, D, C, A = seg_caps.get_shape()
+    y = layers.Input(shape=input_shape[:-1]+(1,))
+    masked_by_y = Mask()([seg_caps, y])  # The true label is used to mask the output of capsule layer. For training
+    masked = Mask()(seg_caps)  # Mask using the capsule with maximal length. For prediction
+
+    def shared_decoder(mask_layer):
+        recon_remove_dim = layers.Reshape((H.value, W.value, D.value, A.value))(mask_layer)
+
+        recon_1 = layers.Conv3D(filters=64, kernel_size=1, padding='same', kernel_initializer='he_normal',
+                                activation='relu', name='recon_1')(recon_remove_dim)
+
+        recon_2 = layers.Conv3D(filters=128, kernel_size=1, padding='same', kernel_initializer='he_normal',
+                                activation='relu', name='recon_2')(recon_1)
+
+        out_recon = layers.Conv3D(filters=1, kernel_size=1, padding='same', kernel_initializer='he_normal',
+                                  activation='sigmoid', name='out_recon')(recon_2)
+
+        return out_recon
+
+    # Models for training and evaluation (prediction)
+    train_model = models.Model(inputs=[x, y], outputs=[out_seg, shared_decoder(masked_by_y)])
+    eval_model = models.Model(inputs=x, outputs=[out_seg, shared_decoder(masked)])
+
+    # manipulate model
+    noise = layers.Input(shape=((H.value, W.value, D.value, C.value, A.value)))
+    noised_seg_caps = layers.Add()([seg_caps, noise])
+    masked_noised_y = Mask()([noised_seg_caps, y])
+    manipulate_model = models.Model(inputs=[x, y, noise], outputs=shared_decoder(masked_noised_y))
+
+    return train_model, eval_model, manipulate_model
